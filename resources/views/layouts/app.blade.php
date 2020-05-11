49f5b7af5869839902f963dc62d48c78d256f046
Askariot <magicjoke@gmail.com>
2020-05-11 13:08:22 +0300

    11.05 #4

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -41,7 +41,7 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/all_tables') }}">
-                    {{ config('app.name', 'Laravel') }}
+                    {{ config('app.name', 'Агробаза') }}
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
