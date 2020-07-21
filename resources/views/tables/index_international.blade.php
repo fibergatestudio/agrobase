260043b1bae27c3ace2388374395d5d1a9c3e3e5
Askariot <magicjoke@gmail.com>
2020-07-21 15:50:31 +0300

    21.07 #10

--- resources/views/tables/index_international.blade.php
+++ resources/views/tables/index_international.blade.php
@@ -128,8 +128,10 @@
                                             <?php $column_count++; ?>
                                             @endif
                                         <?php } else if (preg_match("/(([\w]+:)?\/\/)?(([\d\w]|%[a-fA-f\d]{2,2})+(:([\d\w]|%[a-fA-f\d]{2,2})+)?@)?([\d\w][-\d\w]{0,253}[\d\w]\.)+[\w]{2,63}(:[\d]+)?(\/([-+_~.\d\w]|%[a-fA-f\d]{2,2})*)*(\?(&?([-+_~.\d\w]|%[a-fA-f\d]{2,2})=?)*)?(#([-+_~.\d\w]|%[a-fA-f\d]{2,2})*)?/", $row->$column) ) { ?>
-                                            <a href="{{ $row->$column }}">{{ $row->$column }}</a>
-                                            <?php $column_count++; ?>
+                                            <?php if($column_count >= 4) { ?>
+                                                <a href="{{ $row->$column }}">{{ $row->$column }}</a>
+                                                <?php $column_count++; ?>
+                                            <?php } ?>
                                         <?php } else { ?>
                                             {{ $row->$column }}
                                             <?php $column_count++; ?>
